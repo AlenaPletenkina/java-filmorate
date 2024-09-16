0f61df6d0a6c89a2d449c972a50e74f2b8398a1f
Alena Pletenkina <alenapletenkina@yandex.ru>
2024-09-16 19:22:27 +0300

    Спринт 12.2 добавила базу данных. Исправила checkStyle.11

--- src/test/java/ru/yandex/practicum/filmorate/storage/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/UserDbStorageTest.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
