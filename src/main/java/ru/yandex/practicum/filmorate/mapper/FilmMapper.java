609335e296043f7099d0382cb47ff33660b9a9be
User Podsekina <margaritapodsekina@gmail.com>
2024-09-30 23:07:42 +0300

    Новые изменения

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
@@ -18,20 +18,17 @@ public class FilmMapper implements RowMapper<Film> {
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
         Rating mpa = new Rating(rs.getInt("rating_id"), rs.getString("rating_name"));
-        Set<Long> likes = new LinkedHashSet<>();
-        Set<Genre> genres = new LinkedHashSet<>();
-        Set<Director> directors = new LinkedHashSet<>();
 
         Film film = Film.builder()
-                .name(rs.getString("film_name"))
                 .id(rs.getInt("film_id"))
+                .name(rs.getString("film_name"))
                 .description(rs.getString("description"))
                 .releaseDate(rs.getDate("release_date").toLocalDate())
                 .duration(rs.getInt("duration"))
                 .mpa(mpa)
                 .likes(rs.getInt("rate"))
-                .genres(genres)
-                .directors(directors)
+                .genres(new LinkedHashSet<>()) // Initialize empty sets; genres will be set later
+                .directors(new LinkedHashSet<>())
                 .build();
 
         return film;
